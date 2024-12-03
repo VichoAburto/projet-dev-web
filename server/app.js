104e238d2ca70b7c2c7dc3355359031424977cfa
Vicente Aburto Olguín <vicente_aburto@Vichos-MacBook-Pro.local>
2024-12-03 10:43:42 +0100

    ADD: Initial commit

---
+++ server/app.js
@@ -0,0 +1,39 @@
+import express from 'express'
+
+const app = express()
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+let donnée = {
+    valeur: 42
+}
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json(donnée);
+
+})
+
+
+app.post('/', (req, res) => {
+    // console.log(req.body)
+
+    donnée.valeur = req.body.valeur;
+    console.log(donnée.valeur);
+    res.redirect('/');
+
+})
+
+
+app.listen(port, hostname);
+console.log(`Server running at http://${hostname}:${port}/`);
+
+
+import { Sequelize } from 'sequelize';
+
+const db = new Sequelize({
+  dialect: 'sqlite',
+  storage: ':memory:',
+});
